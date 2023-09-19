5699b60b52aa56c6fb7f51e98e8e879c9927110d
Fazliddin Farkhodov <103936585+AmoreForever@users.noreply.github.com>
2023-09-19 12:21:16 +0500

    Create math_tasks.py

---
+++ math_tasks.py
@@ -0,0 +1,28 @@
+# list1 = [2, 7, 5, 64, 14]
+# even_count = 0
+# odd_count = 0
+
+# for i in list1:
+#     if i % 2 == 0:
+#         even_count += 1
+#     else:
+#         odd_count += 1
+
+# print(f"Even: {even_count}\nOdd: {odd_count}")
+
+
+# print([i for i in [12, -7, 5, 64, -14] if i < 0])
+
+
+# list_ = [4, 5, 1, 2, 9, 7, 10, 8]
+# sum_ = 0
+# avarage = 0
+
+# for i in list_:
+#     sum_ += i
+#     avarage = sum_ / len(list_)
+
+# print(avarage)
+# print(sum_)
+
+# print(sum(i*i for i in range(1, 4+1)))
